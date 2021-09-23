13d49664066e794b02f06d182ed6eedd773f2658
ngocbao1512 <tranngocbao089@gmail.com>
2021-09-23 21:17:27 +0700

    add feature login

--- Ngoc_Bao/myweb/routes/web.php
+++ Ngoc_Bao/myweb/routes/web.php
@@ -1,14 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\Admin\ProductController as AdminProductController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
 
 
 Route::get('/home-page', [HomeController::class, 'index'])
